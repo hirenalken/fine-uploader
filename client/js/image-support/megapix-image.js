fe0e3d00129941726cc8af878a71517b62ebda6e
Hiren Patel <hiren@auberginesolutions.com>
2017-07-07 18:49:24 +0530

    fixed - removing target.onload() for img element

--- client/js/image-support/megapix-image.js
+++ client/js/image-support/megapix-image.js
@@ -383,7 +383,7 @@
                 renderImageToDataURL(self.srcImage, self.blob, opt, doSquash)
                     .then(function(dataUri) {
                         target.src = dataUri;
-                        oldTargetSrc === target.src && target.onload();
+                        oldTargetSrc === target.src;
                     });
             }());
         } else if (tagName === "canvas") {
